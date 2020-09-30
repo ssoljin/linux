1e67575eba2820403342971664ca94e69d81ab82
ssoljin <soljin8122@naver.com>
2020-10-01 01:44:58 +0900

    Change code

--- assignment3.sh
+++ assignment3.sh
@@ -1,4 +1,16 @@
 #!/bin/sh
+if [ $1 -le 0 ]
+then
+	echo "$1 is not natural number..."
+	exit 0
+fi
+
+if [ $2 -le 0 ]
+then
+	echo "$2 is not natural number..."
+	exit 0
+fi
+
 for i in $(seq 1 $1)
 do
 	str=
@@ -10,3 +22,4 @@ do
 	done
 	echo $str
 done
+exit 0
